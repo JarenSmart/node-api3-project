d1e6922a4e700621b1a37d33c5d4db91d115d91a
jaren <jarensmart@yahoo.com>
2020-06-11 02:27:11 -0400

    added server info/logger/add user/get users/delete user

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,93 @@
-const express = require('express');
+const express = require("express");
+const userDb = require("./userDb");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", validateUser(), (req, res, next) => {
+  userDb
+    .insert(req.body)
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch(next);
+});
+
+router.post("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.get("/", (req, res, next) => {
+  userDb
+    .get()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.get("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.get("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.delete("/:id", validateUserId(), (req, res, next) => {
+  userDb
+    .remove(req.params.id)
+    .then(() => {
+      res.status(200).json({
+        message: "The user was removed successfully",
+      });
+    })
+    .catch(next);
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
 //custom middleware
 
-function validateUserId(req, res, next) {
-  // do your magic!
+function validateUserId() {
+  return (req, res, next) => {
+    userDb
+      .getById(req.params.id)
+      .then((user) => {
+        if (user) {
+          req.user = user;
+          next();
+        } else {
+          res.status(400).json({
+            message: "Invalid user id",
+          });
+        }
+      })
+      .catch(next);
+  };
 }
 
-function validateUser(req, res, next) {
-  // do your magic!
+function validateUser() {
+  return (req, res, next) => {
+    if (!req.body) {
+      return res.status(400).json({
+        message: "missing user data",
+      });
+    } else if (!req.body.name) {
+      return res.status(400).json({
+        message: "missing required name field",
+      });
+    }
+
+    next();
+  };
 }
 
-function validatePost(req, res, next) {
-  // do your magic!
-}
+// function validatePost(req, res, next) {
+//   // do your magic!
+// }
 
 module.exports = router;
